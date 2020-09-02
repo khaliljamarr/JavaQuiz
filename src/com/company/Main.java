1ec78a2684e977318bd8458d51f777dd2d61a1ca
Khalil Jamarr <KhalilEdmondson@KhalilJamarr.local>
2020-09-02 16:15:06 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    int test_score = 95;
+    String name = "Khalil Edmondson";
+    double gpa = 4.00;
+    boolean t = true;
+
+    System.out.println("Your test score was: " + test_score);
+    System.out.println("Hello " + name + " , how are you?" );
+    System.out.println(name + " your GPA is a " + gpa );
+    System.out.println("True or False: Today is Wednesday? " + t);
+
+	// write your code here
+        //int
+        //String
+        //float
+        //boolean
+        //declare each type of variable
+        //give it a value
+        //output that value as part of a sentence
+    }
+}
